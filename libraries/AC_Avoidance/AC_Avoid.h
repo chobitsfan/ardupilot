bd9b5b998b9e04e8ddcb80787950322b5e5df18d
chobits <chobits@itri.org.tw>
2020-02-27 16:47:56 +0800

    support circle radius in poshold

--- libraries/AC_Avoidance/AC_Avoid.h
+++ libraries/AC_Avoidance/AC_Avoid.h
@@ -145,6 +145,8 @@ private:
     // returns the maximum positive and negative roll and pitch percentages (in -1 ~ +1 range) based on the proximity sensor
     void get_proximity_roll_pitch_pct(float &roll_positive, float &roll_negative, float &pitch_positive, float &pitch_negative);
 
+    void get_fence_roll_pitch_pct(float &roll_positive, float &roll_negative, float &pitch_positive, float &pitch_negative);
+
     // parameters
     AP_Int8 _enabled;
     AP_Int16 _angle_max;        // maximum lean angle to avoid obstacles (only used in non-GPS flight modes)
