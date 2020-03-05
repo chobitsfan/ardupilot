5208a4865c766923168d1cf5fa11853d7c48c595
chobits <chobits@itri.org.tw>
2020-03-05 16:10:43 +0800

    support polygon fence in poshold (not tested yet)

--- libraries/AC_Avoidance/AC_Avoid.cpp
+++ libraries/AC_Avoidance/AC_Avoid.cpp
@@ -860,47 +860,88 @@ float AC_Avoid::distance_to_lean_pct(float dist_m)
 
 bool AC_Avoid::get_fence_roll_pitch_pct(float &roll_positive, float &roll_negative, float &pitch_positive, float &pitch_negative)
 {    
-    AC_Fence *fence = AP::fence();
+    const AC_Fence *fence = AP::fence();
     if (fence == nullptr) {
         return false;
     }
 
-    // exit if circular fence is not enabled
-    if ((fence->get_enabled_fences() & AC_FENCE_TYPE_CIRCLE) == 0) {
-        return false;
-    }
-
-    // exit if the circular fence has already been breached
-    if ((fence->get_breaches() & AC_FENCE_TYPE_CIRCLE) != 0) {
-        return false;
-    }
-    const float fence_radius = fence->get_radius();
-    const float margin = fence->get_margin();
-
     bool adjusted = false;
     const AP_AHRS &_ahrs = AP::ahrs();
-    Vector2f pos; 
-    if (_ahrs.get_relative_position_NE_origin(pos)) {
-        float dist = fence_radius - pos.length();
-        if (dist < margin) {
-            const float lean_pct = distance_to_lean_pct(dist);
-            const float angle_rad = pos.angle() - _ahrs.yaw;
-            const float roll_pct = -sinf(angle_rad) * lean_pct;
-            const float pitch_pct = cosf(angle_rad) * lean_pct;
-            // update roll, pitch maximums
-            if (roll_pct > 0.0f) {
-                roll_positive = MAX(roll_positive, roll_pct);
-            } else if (roll_pct < 0.0f) {
-                roll_negative = MIN(roll_negative, roll_pct);
+
+    uint8_t enabled_fences = fence->get_enabled_fences();
+    uint8_t breaches = fence->get_breaches();
+
+    if ((enabled_fences & AC_FENCE_TYPE_CIRCLE) && ((breaches & AC_FENCE_TYPE_CIRCLE) == 0)) {
+        const float fence_radius = fence->get_radius();
+        Vector2f pos; 
+        if (_ahrs.get_relative_position_NE_origin(pos)) {
+            float dist = fence_radius - pos.length();
+            if (dist < _dist_max) {
+                const float lean_pct = distance_to_lean_pct(dist);
+                const float angle_rad = pos.angle() - _ahrs.yaw;
+                const float roll_pct = -sinf(angle_rad) * lean_pct;
+                const float pitch_pct = cosf(angle_rad) * lean_pct;
+                // update roll, pitch maximums
+                if (roll_pct > 0.0f) {
+                    roll_positive = MAX(roll_positive, roll_pct);
+                } else if (roll_pct < 0.0f) {
+                    roll_negative = MIN(roll_negative, roll_pct);
+                }
+                if (pitch_pct > 0.0f) {
+                    pitch_positive = MAX(pitch_positive, pitch_pct);
+                } else if (pitch_pct < 0.0f) {
+                    pitch_negative = MIN(pitch_negative, pitch_pct);
+                }
+                adjusted = true;
             }
-            if (pitch_pct > 0.0f) {
-                pitch_positive = MAX(pitch_positive, pitch_pct);
-            } else if (pitch_pct < 0.0f) {
-                pitch_negative = MIN(pitch_negative, pitch_pct);
-            }
-            adjusted = true;
         }
     }
+    
+    if ((enabled_fences & AC_FENCE_TYPE_POLYGON) && ((breaches & AC_FENCE_TYPE_POLYGON) == 0)) {
+        float dist_max_squared = _dist_max * _dist_max;
+        Vector2f pos; 
+        if (_ahrs.get_relative_position_NE_origin(pos)) {
+            const uint8_t num_inclusion_polygons = fence->polyfence().get_inclusion_polygon_count();
+            for (uint8_t i = 0; i < num_inclusion_polygons; i++) {
+                uint16_t num_points;
+                const Vector2f* boundary = fence->polyfence().get_inclusion_polygon(i, num_points);
+                if (boundary == nullptr || num_points < 3) {
+                    continue;
+                }
+                for (uint16_t k=0; k<num_points; k++) {
+                    uint16_t j = k+1;
+                    if (j >= num_points) {
+                        j = 0;
+                    }
+                    // end points of current edge
+                    Vector2f start = boundary[j];
+                    Vector2f end = boundary[k];
+                    Vector2f cp = Vector2f::closest_point(pos, start, end);
+                    Vector2f pos_cp = cp - pos;
+                    float dist_squared = pos_cp.length_squared();
+                    if (dist_squared < dist_max_squared) {
+                        const float lean_pct = distance_to_lean_pct(sqrtf(dist_squared));
+                        const float angle_rad = pos_cp.angle() - _ahrs.yaw;
+                        const float roll_pct = -sinf(angle_rad) * lean_pct;
+                        const float pitch_pct = cosf(angle_rad) * lean_pct;
+                        // update roll, pitch maximums
+                        if (roll_pct > 0.0f) {
+                            roll_positive = MAX(roll_positive, roll_pct);
+                        } else if (roll_pct < 0.0f) {
+                            roll_negative = MIN(roll_negative, roll_pct);
+                        }
+                        if (pitch_pct > 0.0f) {
+                            pitch_positive = MAX(pitch_positive, pitch_pct);
+                        } else if (pitch_pct < 0.0f) {
+                            pitch_negative = MIN(pitch_negative, pitch_pct);
+                        }
+                        adjusted = true;                        
+                    }
+                }
+            }
+        }
+    }
+
     return adjusted;
 }
 
