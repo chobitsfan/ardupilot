53a9749620b49f1c0e6c2d6c74801e52cb8c70f2
chobits <chobits@itri.org.tw>
2020-04-15 09:36:35 +0800

    use distance_sensor for mixed-reality collision feedback

--- libraries/AP_Proximity/AP_Proximity_MAV.h
+++ libraries/AP_Proximity/AP_Proximity_MAV.h
@@ -23,6 +23,9 @@ public:
     // handle mavlink DISTANCE_SENSOR messages
     void handle_msg(const mavlink_message_t &msg) override;
 
+    uint8_t get_object_count() const override;
+    bool get_object_angle_and_distance(uint8_t object_number, float& angle_deg, float &distance) const override;
+
 private:
     // horizontal distance support
     uint32_t _last_update_ms;   // system time of last DISTANCE_SENSOR message received
@@ -32,4 +35,7 @@ private:
     // upward distance support
     uint32_t _last_upward_update_ms;    // system time of last update distance
     float _distance_upward;             // upward distance in meters
+
+    float _dist;
+    int _angle_deg;
 };
