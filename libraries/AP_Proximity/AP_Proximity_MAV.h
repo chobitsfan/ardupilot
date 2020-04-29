6f6ae7fd68a58e6dde12fbd2cfee641433b74290
chobits <chobits@itri.org.tw>
2020-04-29 14:31:46 +0800

    use distance_sensor for collision interaction

--- libraries/AP_Proximity/AP_Proximity_MAV.h
+++ libraries/AP_Proximity/AP_Proximity_MAV.h
@@ -23,6 +23,9 @@ public:
     // handle mavlink DISTANCE_SENSOR messages
     void handle_msg(const mavlink_message_t &msg) override;
 
+    uint8_t get_object_count() const override;
+    bool get_object_angle_and_distance(uint8_t object_number, float& angle_deg, float &distance) const override;
+
 private:
 
     // initialise sensor (returns true if sensor is succesfully initialised)
@@ -36,4 +39,7 @@ private:
     // upward distance support
     uint32_t _last_upward_update_ms;    // system time of last update distance
     float _distance_upward;             // upward distance in meters
+
+    float _dist;
+    int _angle_deg;
 };
