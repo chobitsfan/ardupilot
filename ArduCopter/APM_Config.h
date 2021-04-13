129f4989ef88cd4d01c3df9863fac1cbd04810bb
chobits <chobits@itri.org.tw>
2021-04-13 09:45:23 +0800

    disable unused features

--- ArduCopter/APM_Config.h
+++ ArduCopter/APM_Config.h
@@ -9,22 +9,22 @@
 #define AUTOTUNE_ENABLED      DISABLED            // disable the auto tune functionality to save 7k of flash
 //#define AC_FENCE              DISABLED            // disable fence to save 2k of flash
 #define CAMERA                DISABLED            // disable camera trigger to save 1k of flash
-//#define RANGEFINDER_ENABLED   DISABLED            // disable rangefinder to save 1k of flash
+#define RANGEFINDER_ENABLED   DISABLED            // disable rangefinder to save 1k of flash
 #define AC_RALLY              DISABLED            // disable rally points library (must also disable terrain which relies on rally)
-//#define AC_AVOID_ENABLED      DISABLED            // disable stop-at-fence library
+#define AC_AVOID_ENABLED      DISABLED            // disable stop-at-fence library
 #define AC_OAPATHPLANNER_ENABLED DISABLED         // disable path planning around obstacles
 #define AC_TERRAIN            DISABLED            // disable terrain library
 #define PARACHUTE             DISABLED            // disable parachute release to save 1k of flash
 //#define NAV_GUIDED            DISABLED            // disable external navigation computer ability to control vehicle through MAV_CMD_NAV_GUIDED mission commands
-//#define OPTFLOW               DISABLED            // disable optical flow sensor to save 5K of flash space
+#define OPTFLOW               DISABLED            // disable optical flow sensor to save 5K of flash space
 #define FRSKY_TELEM_ENABLED   DISABLED            // disable FRSky telemetry
 #define PRECISION_LANDING     DISABLED            // disable precision landing using companion computer or IRLock sensor
 #define BEACON_ENABLED        DISABLED            // disable beacon support
 #define SPRAYER_ENABLED       DISABLED            // disable the crop sprayer feature (two ESC controlled pumps the speed of which depends upon the vehicle's horizontal velocity)
 #define WINCH_ENABLED         DISABLED            // disable winch support
 #define GRIPPER_ENABLED       DISABLED            // disable gripper support
-//#define RPM_ENABLED           DISABLED            // disable rotations per minute sensor support
-//#define STATS_ENABLED         DISABLED            // disable statistics support
+#define RPM_ENABLED           DISABLED            // disable rotations per minute sensor support
+#define STATS_ENABLED         DISABLED            // disable statistics support
 #define MODE_ACRO_ENABLED     DISABLED            // disable acrobatic mode support
 //#define MODE_AUTO_ENABLED     DISABLED            // disable auto mode support
 //#define MODE_BRAKE_ENABLED    DISABLED            // disable brake mode support
