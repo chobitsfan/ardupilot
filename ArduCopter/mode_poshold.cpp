d459af7518d0f1315ca156ca00e4d259caef8552
chobitsfan <chobits@itri.org.tw>
2021-06-08 14:45:12 +0800

    master code change

--- ArduCopter/mode_poshold.cpp
+++ ArduCopter/mode_poshold.cpp
@@ -70,9 +70,9 @@ bool ModePosHold::brake_at_fence(float target_pitch, float target_roll)
     if (fence && fence->enabled()) {
         const Vector3f& vel = inertial_nav.get_velocity();
         if (vel.x * vel.x + vel.y * vel.y > 900.0f) {
-            Vector3f pos_cm;
-            pos_control->get_stopping_point_xy(pos_cm);
-            if (fence->polyfence().breached(Vector2f(pos_cm.x, pos_cm.y))) {
+            Vector2p pos_cm;
+            pos_control->get_stopping_point_xy_cm(pos_cm);
+            if (fence->polyfence().breached(pos_cm.tofloat())) {
                 return true;
             }
         } else {
@@ -91,7 +91,7 @@ float ModePosHold::get_fence_adjusted_climbrate(float target_rate) {
     AC_Fence *fence = AP::fence();
     if (fence && fence->enabled()) {
         float kP = pos_control->get_pos_z_p().kP();
-        float accel_cmss = pos_control->get_max_accel_z();
+        float accel_cmss = pos_control->get_max_accel_z_cmss();
 
         // do not adjust climb_rate if level
         if (is_zero(target_rate)) {
