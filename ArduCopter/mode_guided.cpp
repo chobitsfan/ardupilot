db8c7786345c52cd0f5f74ab1b5aab2323a0c941
chobits <chobits@itri.org.tw>
2021-06-16 17:40:16 +0800

    use local ned to check fence breach

--- ArduCopter/mode_guided.cpp
+++ ArduCopter/mode_guided.cpp
@@ -231,8 +231,7 @@ bool ModeGuided::set_destination(const Vector3f& destination, bool use_yaw, floa
 {
 #if AC_FENCE == ENABLED
     // reject destination if outside the fence
-    const Location dest_loc(destination, terrain_alt ? Location::AltFrame::ABOVE_TERRAIN : Location::AltFrame::ABOVE_ORIGIN);
-    if (!copter.fence.check_destination_within_fence(dest_loc)) {
+    if (!copter.fence.check_destination_within_fence(destination)) {
         AP::logger().Write_Error(LogErrorSubsystem::NAVIGATION, LogErrorCode::DEST_OUTSIDE_FENCE);
         // failure is propagated to GCS with NAK
         return false;
@@ -330,8 +329,7 @@ bool ModeGuided::set_destination_posvel(const Vector3f& destination, const Vecto
 {
 #if AC_FENCE == ENABLED
     // reject destination if outside the fence
-    const Location dest_loc(destination, Location::AltFrame::ABOVE_ORIGIN);
-    if (!copter.fence.check_destination_within_fence(dest_loc)) {
+    if (!copter.fence.check_destination_within_fence(destination)) {
         AP::logger().Write_Error(LogErrorSubsystem::NAVIGATION, LogErrorCode::DEST_OUTSIDE_FENCE);
         // failure is propagated to GCS with NAK
         return false;
