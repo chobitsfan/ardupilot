1cc460a83ab7069f262dab98a23cf8a2c865d617
chobits <chobits@itri.org.tw>
2022-03-22 14:20:19 +0800

    increase avd rate

--- ArduCopter/Copter.cpp
+++ ArduCopter/Copter.cpp
@@ -189,7 +189,7 @@ const AP_Scheduler::Task Copter::scheduler_tasks[] = {
     SCHED_TASK_CLASS(Compass, &copter.compass, cal_update, 100, 100, 132),
     SCHED_TASK_CLASS(AP_TempCalibration,   &copter.g2.temp_calibration, update,          10, 100, 135),
 #if HAL_ADSB_ENABLED
-    SCHED_TASK(avoidance_adsb_update, 10,    100, 138),
+    SCHED_TASK(avoidance_adsb_update, 40,    100, 138),
 #endif
 #if ADVANCED_FAILSAFE == ENABLED
     SCHED_TASK(afs_fs_check,          10,    100, 141),
